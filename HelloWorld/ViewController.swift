b50826647b0f736d17f60a946b610dad1f923c6e
Nikk <App.blog22@gmail.com>
2022-07-18 17:46:17 +0300

    Add hide and show features

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var helloWorldLabel: UILabel!
+    @IBOutlet var toggleButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        helloWorldLabel.isHidden = true
+        
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func makeButtonAction(_ sender: Any) {
+        if helloWorldLabel.isHidden {
+            helloWorldLabel.isHidden = false
+            toggleButton.setTitle("Hide Text", for: .normal)
+        } else {
+            helloWorldLabel.isHidden = true
+            toggleButton.setTitle("Show Text", for: .normal)
+        }
+    }
+    
 }
 
